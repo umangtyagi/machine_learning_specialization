8519a4cfb96638a8af40f0ecef5a5d033ff4b95c
Umang Tyagi <umangtyagi1802@gmail.com>
2024-01-06 19:40:26 +0530

    updated comments in utils.py

--- digit_recognition/utils.py
+++ digit_recognition/utils.py
@@ -5,7 +5,7 @@ import matplotlib.image as mpimg
 
 def plot_random_images(data_dir):
     """
-    Plot random images from the dataset.
+    Plots random images from the dataset.
 
     Parameters:
     - data_dir (str): The path to the main dataset directory.
@@ -13,37 +13,35 @@ def plot_random_images(data_dir):
     Returns:
     - None
     """
-    # Create a 10x5 grid of subplots for displaying images
+    # create a 10x5 grid of subplots for displaying images
     fig, ax = plt.subplots(10, 5, figsize=(40, 15))
 
-    # Loop through each digit (0 to 9)
+    # loop through each digit (0 to 9)
     for digit in range(10):
-
-        # Directory for each digit's dataset
+        # directory for each digit's dataset
         digit_dir = os.path.join(data_dir, str(digit))
 
-        # Make a filelist of images with the '.jpg' extension
+        # make a filelist of images with the '.jpg' extension
         filelist = [image for image in os.listdir(digit_dir) if image.endswith('.jpg')]
 
-        # Pick 5 random images from the current digit's dataset
+        # pick 5 random images from the current digit's dataset
         random_images = random.sample(filelist, 5)
 
-        # Display the randomly chosen images for the current digit
+        # display the randomly chosen images for the current digit
         for i, image_name in enumerate(random_images):
-
-            # Construct the full path to the image
+            # construct the full path to the image
             image_path = os.path.join(digit_dir, image_name)
 
-            # Read the image using matplotlib.image
+            # read the image using matplotlib.image
             img = mpimg.imread(image_path)
 
-            # Display the image in the current subplot
+            # display the image in the current subplot
             ax[digit, i].imshow(img)
             ax[digit, i].set_title(image_name)
             ax[digit, i].axis('off')
 
-    # Adjust layout for better spacing
+    # adjust layout for better spacing
     plt.tight_layout()
 
-    # Show the plot with all 5 images for each digit
+    # show the plot with all 5 images for each digit
     plt.show()
